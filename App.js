de48e7394347ef74a166758f08ed0f3d04c9c5b2
PratikKshirsagar <meetpratiik@gmail.com>
2020-04-21 12:36:36 +0530

    header and footer setup done

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1 }}>
+      <SafeAreaView />
+      <View
+        style={{
+          height: 70,
+          borderBottomWidth: 0.5,
+          borderBottomColor: '#E9E9E9',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text style={{ fontSize: 24 }}>Book Worm</Text>
+      </View>
+      <View style={{ flex: 1 }}></View>
+      <View
+        style={{
+          height: 70,
+          borderTopWidth: 0.5,
+          borderTopColor: '#E9E9E9',
+          flexDirection: 'row',
+        }}
+      >
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ fontSize: 20 }}>Total</Text>
+          <Text>0</Text>
+        </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ fontSize: 20 }}>Reading</Text>
+          <Text>0</Text>
+        </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ fontSize: 20 }}>Read</Text>
+          <Text>0</Text>
+        </View>
+      </View>
+      <SafeAreaView />
     </View>
   );
 }
@@ -12,8 +46,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
