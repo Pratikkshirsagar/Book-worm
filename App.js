272c1bdf480202ce51ab6e514d375fb4185e05e4
PratikKshirsagar <meetpratiik@gmail.com>
2020-04-22 14:16:07 +0530

    app drawer added

--- App.js
+++ App.js
@@ -3,14 +3,42 @@ import React from 'react';
 import WelcomeScreen from './screens/AppSwitchNavigator/WelcomeScreen';
 import HomeScreen from './screens/HomeScreen';
 import SignUpScreen from './screens/SignUpScreen';
-import { createSwitchNavigator, createAppContainer, createStackNavigator } from 'react-navigation';
+import {
+  createSwitchNavigator,
+  createAppContainer,
+  createStackNavigator,
+  createDrawerNavigator,
+} from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
 
 const App = () => <AppContainer />;
 
+const loginStackNavigator = createStackNavigator({
+  WelcomeScreen: {
+    screen: WelcomeScreen,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  SignUpScreen: {
+    screen: SignUpScreen,
+    navigationOptions: {},
+  },
+});
+
+const AppDrawerNavigator = createDrawerNavigator({
+  HomeScreen: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: 'Home',
+      drawerIcon: () => <Ionicons name="ios-home" size={24} />,
+    },
+  },
+});
+
 const AppSwitchNavigator = createSwitchNavigator({
-  WelcomeScreen,
-  HomeScreen,
-  SignUpScreen,
+  loginStackNavigator,
+  AppDrawerNavigator,
 });
 
 const AppContainer = createAppContainer(AppSwitchNavigator);
