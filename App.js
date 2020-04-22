ace2028065a34c32bfb9bbb16c326df4a2529634
PratikKshirsagar <meetpratiik@gmail.com>
2020-04-22 13:19:25 +0530

    login screen added

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import WelcomeScreen from './screens/AppSwitchNavigator/WelcomeScreen';
+import HomeScreen from './screens/HomeScreen';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 
 const App = () => <AppContainer />;
 
 const AppSwitchNavigator = createSwitchNavigator({
   WelcomeScreen,
+  HomeScreen,
 });
 
 const AppContainer = createAppContainer(AppSwitchNavigator);
